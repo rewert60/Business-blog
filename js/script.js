50dc7de739d56fd762362c1afd126d9e68624076
Yan <rewert60@gmail.com>
2016-01-24 19:24:13 +0200

    responsive design

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    
+    $('#nav-icon').toggle (
+  function() {
+    $("#block-content-right").fadeIn(200);
+  },
+  function() {
+    $("#block-content-right").fadeOut(200);
+  });
+  
+ $("#blog-categories").click(function(){
+        
+        $("#category-list").slideToggle(200);               
+        
+    });    
+
+ $("#blog-archives").click(function(){
+        
+        $("#archives-list").slideToggle(200);               
+        
+    }); 
+       
+});
